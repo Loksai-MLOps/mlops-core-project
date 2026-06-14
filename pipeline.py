3c3eddfece09cefd275118dac6e4da077f46afea
Loksai <62761750+LoksaiETA@users.noreply.github.com>
2026-06-14 14:15:07 +0530

    Create Kubeflow pipeline with validation and training ops
    This script defines a Kubeflow pipeline with components for validation, preprocessing, and training. Each component clones a GitHub repository, downloads data from S3, and executes respective Python scripts.

---
+++ pipeline.py
@@ -0,0 +1,122 @@
+from kfp import dsl
+from kfp import compiler
+
+
+@dsl.component(
+    base_image="python:3.10",
+    packages_to_install=["boto3", "pandas", "scikit-learn", "mlflow", "joblib"]
+)
+def validate_op():
+
+    import os
+    import boto3
+
+    REPO = "https://github.com/Loksai-MLOps/mlops-core-project.git"
+
+    os.system(f"git clone {REPO}")
+
+    s3 = boto3.client("s3")
+
+    os.makedirs(
+        "mlops-core-project/data/raw",
+        exist_ok=True
+    )
+
+    s3.download_file(
+        "loksai-edu-mlproject1",
+        "reviews.csv",
+        "mlops-core-project/data/raw/reviews.csv"
+    )
+
+    os.chdir("mlops-core-project")
+
+    os.system("python src/data_validation.py")
+
+
+@dsl.component(
+    base_image="python:3.10",
+    packages_to_install=["boto3", "pandas", "scikit-learn"]
+)
+def preprocess_op():
+
+    import os
+    import boto3
+
+    REPO = "https://github.com/Loksai-MLOps/mlops-core-project.git"
+
+    os.system(f"git clone {REPO}")
+
+    s3 = boto3.client("s3")
+
+    os.makedirs(
+        "mlops-core-project/data/raw",
+        exist_ok=True
+    )
+
+    s3.download_file(
+        "loksai-edu-mlproject1",
+        "reviews.csv",
+        "mlops-core-project/data/raw/reviews.csv"
+    )
+
+    os.chdir("mlops-core-project")
+
+    os.system("python src/preprocessing.py")
+
+
+@dsl.component(
+    base_image="python:3.10",
+    packages_to_install=["boto3", "pandas", "scikit-learn", "mlflow", "joblib"]
+)
+def train_op():
+
+    import os
+    import boto3
+
+    REPO = "https://github.com/Loksai-MLOps/mlops-core-project.git"
+
+    os.system(f"git clone {REPO}")
+
+    s3 = boto3.client("s3")
+
+    os.makedirs(
+        "mlops-core-project/data/raw",
+        exist_ok=True
+    )
+
+    s3.download_file(
+        "loksai-edu-mlproject1",
+        "reviews.csv",
+        "mlops-core-project/data/raw/reviews.csv"
+    )
+
+    os.chdir("mlops-core-project")
+
+    os.makedirs(
+        "models",
+        exist_ok=True
+    )
+
+    os.system("python src/preprocessing.py")
+
+    os.system("python src/train.py")
+
+
+@dsl.pipeline(
+    name="mlops-core-learning-pipeline"
+)
+def pipeline():
+
+    validate = validate_op()
+
+    preprocess = preprocess_op().after(validate)
+
+    train_op().after(preprocess)
+
+
+if __name__ == "__main__":
+
+    compiler.Compiler().compile(
+        pipeline_func=pipeline,
+        package_path="pipeline.yaml"
+    )
